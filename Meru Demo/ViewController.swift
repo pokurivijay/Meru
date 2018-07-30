48e26e0416327c10f86f1c2438e87dd0dd001d02
Vijay <vijay@Inspira-mac-mini.local>
2018-07-30 10:31:33 +0530

    Initial Commit

---
+++ Meru Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meru Demo
+//
+//  Created by Vijay on 30/07/18.
+//  Copyright © 2018 VJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
