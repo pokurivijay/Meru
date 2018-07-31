9ff1591cd8de5403310c2f2aca8bba16b72c1494
Vijay <vijay@Inspira-mac-mini.local>
2018-07-31 10:37:37 +0530

    Shared schema

---
+++ Meru Demo/Services/AuthService.swift
@@ -0,0 +1,9 @@
+//
+//  AuthService.swift
+//  Meru Demo
+//
+//  Created by Vijay on 31/07/18.
+//  Copyright © 2018 VJ. All rights reserved.
+//
+
+import Foundation
---
+++ Meru Demo/Services/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  Meru Demo
+//
+//  Created by Vijay on 31/07/18.
+//  Copyright © 2018 VJ. All rights reserved.
+//
+
+import Foundation
